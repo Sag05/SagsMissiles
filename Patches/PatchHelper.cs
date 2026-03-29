1acd7d4bc7bf6d5d706b8a2c8d6e67f94ce056f6
VenomiXeD <crafterious@gmail.com>
2026-03-29 15:52:36 +0200

    Introduce Transpiler for the MissileMovementControl system in FtD.
    Currently patches sideslip forces, but it is better with an overhaul in the future

---
+++ Patches/PatchHelper.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using BrilliantSkies.Core.Logger;
+using HarmonyLib;
+
+namespace SagsMissiles
+{
+    public class PatchHelper
+    {
+        public static bool SlidingWindowMatch(List<CodeInstruction> codes, OpCode[] reference, int startIndex) => SlidingWindowMatch(codes.ToArray(),reference, startIndex);
+
+        public static bool SlidingWindowMatch(CodeInstruction[] codes, OpCode[] reference, int startIndex)
+        {
+
+            
+            if (startIndex + reference.Length > codes.Length)
+                return false;
+            
+            for (int i = 0; i < reference.Length; i++)
+            {
+                if (codes[startIndex + i].opcode != reference[i])
+                    return false;
+            }
+
+            AdvLogger.LogInfo(
+                $"[PatchHelper.SlidingWindowMatch] MATCH:\n{string.Join('\n',reference.Select(e=>e.Name + ";"))}" +
+                $"\n\nAGAINST:\n{string.Join('\n', codes.Select(e=>e.opcode.Name + ";"))}\n\nINDEX: {startIndex}"
+                
+            );
+            return true;
+        }
+    }
+}
---
+++ Patches/Patch_MissileMovementControl.cs
@@ -0,0 +1,80 @@
+﻿using System.CodeDom;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using BrilliantSkies.Ftd.Missiles.Blueprints;
+using HarmonyLib;
+
+using static System.Reflection.Emit.OpCodes;
+
+namespace SagsMissiles
+{
+    [HarmonyPatch(typeof(MissileMovementControl))]
+    public class Patch_MissileMovementControl
+    {
+        private static readonly OpCode[] AddForceCodeInstructions =
+        {
+            Ldarg_0,
+            Callvirt,
+            Ldloc_S,
+            Ldarg_0,
+            Callvirt,
+            Callvirt,
+            Call,
+            Callvirt,
+            Nop
+        };
+        
+        [HarmonyPatch("ConvertVelocityWithFins")]
+        [HarmonyTranspiler]
+        static IEnumerable<CodeInstruction> Modify_ConvertVelocityWithFins(IEnumerable<CodeInstruction> instructions)
+        {
+            /* REMOVE:
+             * IL_0129: ldarg.0      // missile 
+             * IL_012a: callvirt     instance class [UnityEngine.PhysicsModule]UnityEngine.Rigidbody [Core]BrilliantSkies.Core.UniverseRepresentation.ThreadedGameObject::get_Rigidbody() 
+             * IL_012f: ldloc.s      vector3_3 
+             * IL_0131: ldarg.0      // missile 
+             * IL_0132: callvirt     instance class [UnityEngine.PhysicsModule]UnityEngine.Rigidbody [Core]BrilliantSkies.Core.UniverseRepresentation.ThreadedGameObject::get_Rigidbody() 
+             * IL_0137: callvirt     instance float32 [UnityEngine.PhysicsModule]UnityEngine.Rigidbody::get_mass() 
+             * IL_013c: call         valuetype [UnityEngine.CoreModule]UnityEngine.Vector3 [UnityEngine.CoreModule]UnityEngine.Vector3::op_Multiply(valuetype [UnityEngine.CoreModule]UnityEngine.Vector3, float32) 
+             * IL_0141: callvirt     instance void [UnityEngine.PhysicsModule]UnityEngine.Rigidbody::AddForce(valuetype [UnityEngine.CoreModule]UnityEngine.Vector3) 
+             * IL_0146: nop
+             *
+             * 
+             */
+            List<CodeInstruction> codes = instructions.ToList();
+
+
+            
+            int i = 0;
+            int addForceEncounter = 0;
+            while (i < codes.Count)
+            {
+                if (PatchHelper.SlidingWindowMatch(codes, AddForceCodeInstructions, i))
+                {
+                    if (++addForceEncounter == 2)
+                    {
+                        i += AddForceCodeInstructions.Length;
+                        continue;
+                    }
+                }
+
+                if (codes[i].opcode == Call && 
+                    codes[i].operand is MethodInfo { Name: "TwoPoints" } && 
+                    (codes.ElementAtOrDefault(i+1)?.IsLdloc() ?? false))
+                {
+                    yield return codes[i++];
+                    yield return codes[i++];
+                    yield return codes[i++];
+
+                    yield return new CodeInstruction(Ldc_R4, 10f);
+                    yield return new CodeInstruction(Mul);
+                }
+
+                yield return codes[i++];
+            }
+        }
+    }
+}
